97c87e95e7b58819df11cb29d8e62691099a233d
Karthik Nagaraj <ankarthikengg@gmail.com>
2024-06-08 21:31:35 +0530

    Implemented logout button and updated header text

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Entry } from '../interfaces/entry.interface';
 import { DataService } from '../services/data.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-tab2',
@@ -12,11 +13,16 @@ export class Tab2Page implements OnInit {
   entries: Entry[] = [];
 
   constructor(
-    private dataService: DataService
+    private dataService: DataService,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
     this.entries = this.dataService.getEntries();
+  }
+
+  logout() {
+    this.authService.openConfirmLogoutAlert();
   }
 
 }
