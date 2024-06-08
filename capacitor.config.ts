c1d60477659612fd468fb7daf4ee700954763a51
Karthik Nagaraj <ankarthikengg@gmail.com>
2024-06-08 22:09:43 +0530

    Updated app icon and splashscreen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'temple-accounts',
+  appName: 'Temple Accounts',
   webDir: 'www'
 };
 
